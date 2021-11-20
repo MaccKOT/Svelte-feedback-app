be557ab2236a9334e3d24472397ab3455c29f0c9
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-11-20 17:16:46 +0400

    Add simple svelte builtin animations

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,11 +1,15 @@
 <script>
   import FeedbackItem from './FeedbackItem.svelte';
+  import { fade, scale } from 'svelte/transition';
+
   export let feedback = [];
 
   // console.log(feedback);
 </script>
 
 {#each feedback as fb (fb.id)}
-  <!-- on:delete-feedback - forward custom event to upper component -->
-  <FeedbackItem item={fb} on:delete-feedback />
+  <div in:scale out:fade={{ duration: 500 }}>
+    <!-- on:delete-feedback - forward custom event to upper component -->
+    <FeedbackItem item={fb} on:delete-feedback />
+  </div>
 {/each}
